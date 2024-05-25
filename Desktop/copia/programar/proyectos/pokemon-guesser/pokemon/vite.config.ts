4c93a1542f390938dbd6c449fe18f23112c003fb
ssuf <ssuf2002@gmail.com>
2024-05-25 11:23:54 -0400

    commit workflow agg

--- Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/vite.config.ts
+++ Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-  ],
+ 
+  ], base:'/pokemon/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

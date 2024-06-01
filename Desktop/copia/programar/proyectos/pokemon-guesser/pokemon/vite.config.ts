44bed3c43c08c5479d14a127380258f2a76be210
ssuf <ssuf2002@gmail.com>
2024-06-01 09:44:52 -0400

    laslocurasdelemperador

--- Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/vite.config.ts
+++ Desktop/copia/programar/proyectos/pokemon-guesser/pokemon/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
  
-  ], base:'/pokemon/',
+  ], base:'/pokejuego/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
